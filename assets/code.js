6e2ddba84ce1c354a1b31ea250749d65a58a52cb
prindom <d.prinzensteiner@gmail.com>
2017-10-20 23:04:57 +0200

    added megaclickers

--- assets/code.js
+++ assets/code.js
@@ -16,12 +16,14 @@ $(document).ready(function(e) {
 
   $("#makeClick").on("click", function() {
     clicks++;
+    play("click");
     updateClicks();
   });
 
   $("body").on("click", "button#makeAutoClicker", function() {
     if (money >= autoClickerCost) {
       autoClickers++;
+      play("pling");
       money -= autoClickerCost;
       updateAutoClickerCost();
     } else {
@@ -30,6 +32,20 @@ $(document).ready(function(e) {
     updateClicks();
     updateMoney();
     updateAutoClicker();
+  });
+
+  $("body").on("click", "button#makeMegaClicker", function() {
+    if (money >= megaClickerCost) {
+      megaClickers++;
+      play("pling");
+      money -= megaClickerCost;
+      updateMegaClickerCost();
+    } else {
+      console.info("Not enough money...");
+    }
+    updateClicks();
+    updateMoney();
+    updateMegaClicker();
   });
 
   $("#sellClicks").on("click", function(e) {
@@ -55,9 +71,18 @@ $(document).ready(function(e) {
   });
 });
 
+function play(string) {
+  let audio = new Audio("assets/sounds/"+string+".mp3");
+  audio.play();
+}
+
 function updateClicks() {
   if (clicks >= 10 && progress == 0) {
     unlockAutoClicker();
+    progress++;
+  }
+  if(autoClickers >= 30 && progress == 1) {
+    unlockMegaClicker();
     progress++;
   }
   $("#realClickCount").text(clicks);
@@ -69,6 +94,11 @@ function updateMoney() {
     $("#makeAutoClicker").prop('disabled', true);
   } else {
     $("#makeAutoClicker").prop('disabled', false);
+  }
+  if (money < megaClickerCost) {
+    $("#makeMegaClicker").prop('disabled', true);
+  } else {
+    $("#makeMegaClicker").prop('disabled', false);
   }
 }
 
@@ -85,10 +115,29 @@ function updateAutoClickerCost() {
   $("#autoClickerCost").text(autoClickerCost);
 }
 
+function updateMegaClicker() {
+    $("#realMegaClickerCount").text(megaClickers);
+}
+
+function unlockMegaClicker() {
+  $("#main").append($("<fieldset id='megaClickers'><legend><b>Megaclickers: </b><span id='realMegaClickerCount'>0</span></legend><button id='makeMegaClicker'>+1 Megaclicker</button><p><b>Cost: </b><span id='megaClickerCost'>"+megaClickerCost+"</span></p></fieldset>"));
+}
+
+function updateMegaClickerCost() {
+  megaClickerCost = Math.round(megaClickerCost * megaClickerCostFact);
+  $("#megaClickerCost").text(megaClickerCost);
+}
+
 function everySecondDo() {
   if (autoClickers > 0) {
     for (let i = 0; i < autoClickers; i++) {
       clicks++;
+      updateClicks();
+    }
+  }
+  if(megaClickers > 0) {
+    for (let i = 0; i < megaClickers; i++) {
+      clicks+=15;
       updateClicks();
     }
   }
@@ -100,7 +149,11 @@ function updateAll() {
   if (progress >= 1) {
     unlockAutoClicker();
   }
+  if(progress >= 2) {
+    unlockMegaClicker();
+  }
   updateAutoClicker();
+  updateMegaClicker();
 }
 
 function saveGame() {
@@ -108,6 +161,8 @@ function saveGame() {
   document.cookie = "money=" + money;
   document.cookie = "autoClickers=" + autoClickers;
   document.cookie = "autoClickerCost=" + autoClickerCost;
+  document.cookie = "megaClickers="+ megaClickers;
+  document.cookie = "megaClickerCost=" + megaClickerCost;
   document.cookie = "progress=" + progress;
 }
 
@@ -117,6 +172,8 @@ function loadGame() {
     money = Number(getCookie("money"));
     autoClickers = Number(getCookie("autoClickers"));
     autoClickerCost = Number(getCookie("autoClickerCost"));
+    megaClickers = Number(getCookie("megaClickers"));
+    megaClickerCost = Number(getCookie("megaClickerCost"));
     progress = Number(getCookie("progress"));
     updateAll();
   }
