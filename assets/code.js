8f895261a9e7c0f29920b7a6263c715e4394be4c
dprinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2018-04-27 16:41:32 +0200

    lol digg

--- assets/code.js
+++ assets/code.js
@@ -2,195 +2,204 @@ clicks = 0;
 autoClickers = 0;
 autoClickerCost = 10;
 autoClickerCostFact = 1.15;
+autoClickerInt = null;
 megaClickers = 0;
 megaClickerCost = 1000;
 megaClickerCostFact = 1.5;
+megaClickerInt = null;
 progress = 0;
 money = 0;
 sellFactor = 0.95;
 
-$(document).ready(function(e) {
-  loadGame();
-  $("#main").show();
-  let secondInterval = setInterval(everySecondDo, 1000);
+$(document).ready(function (e) {
+    loadGame();
+    $("#main").show();
+    let secondInterval = setInterval(everySecondDo, 1000);
 
-  $("#makeClick").on("click", function() {
-    clicks++;
-    play("click");
-    updateClicks();
-  });
+    $("#makeClick").on("click", function () {
+        clicks++;
+        play("click");
+        updateClicks();
+    });
 
-  $("body").on("click", "button#makeAutoClicker", function() {
-    if (money >= autoClickerCost) {
-      autoClickers++;
-      play("pling");
-      money -= autoClickerCost;
-      updateAutoClickerCost();
-    } else {
-      console.info("Not enough money...");
-    }
-    updateClicks();
-    updateMoney();
-    updateAutoClicker();
-  });
+    $("body").on("click", "button#makeAutoClicker", function () {
+        if (money >= autoClickerCost) {
+            autoClickers++;
+            play("pling");
+            money -= autoClickerCost;
+            updateAutoClickerCost();
+        } else {
+            console.info("Not enough money...");
+        }
+        updateClicks();
+        updateMoney();
+        updateAutoClicker();
+    });
 
-  $("body").on("click", "button#makeMegaClicker", function() {
-    if (money >= megaClickerCost) {
-      megaClickers++;
-      play("pling");
-      money -= megaClickerCost;
-      updateMegaClickerCost();
-    } else {
-      console.info("Not enough money ...");
-    }
-    updateClicks();
-    updateMoney();
-    updateMegaClicker();
-  });
+    $("body").on("click", "button#makeMegaClicker", function () {
+        if (money >= megaClickerCost) {
+            megaClickers++;
+            play("pling");
+            money -= megaClickerCost;
+            updateMegaClickerCost();
+        } else {
+            console.info("Not enough money ...");
+        }
+        updateClicks();
+        updateMoney();
+        updateMegaClicker();
+    });
 
-  $("#sellClicks").on("click", function(e) {
-    if (clicks > 0) {
-      money += Math.round(clicks * sellFactor, -2);
-      clicks = 0;
-      updateMoney();
-      updateClicks();
-    } else {
-      console.info("No clicks !");
-    }
-  });
+    $("#sellClicks").on("click", function (e) {
+        if (clicks > 0) {
+            money += Math.round(clicks * sellFactor, -2);
+            clicks = 0;
+            updateMoney();
+            updateClicks();
+        } else {
+            console.info("No clicks !");
+        }
+    });
 
-  $(document).on("keydown", function(e) {
-    if (e.keyCode == 83 && (navigator.platform.match("Mac")
-      ? e.metaKey
-      : e.ctrlKey)) {
-      e.preventDefault();
-      // Process event...
-      saveGame();
-      console.log("saved");
-    }
-  });
+    $(document).on("keydown", function (e) {
+        if (e.keyCode == 83 && (navigator.platform.match("Mac")
+                ? e.metaKey
+                : e.ctrlKey)) {
+            e.preventDefault();
+            // Process event...
+            saveGame();
+            console.log("saved");
+        }
+    });
 });
 
 function play(string) {
-  let audio = new Audio("assets/sounds/"+string+".mp3");
-  audio.play();
+    let audio = new Audio("assets/sounds/" + string + ".mp3");
+    audio.play();
 }
 
 function updateClicks() {
-  if (clicks >= 10 && progress == 0) {
-    unlockAutoClicker();
-    progress++;
-  }
-  if(autoClickers >= 30 && progress == 1) {
-    unlockMegaClicker();
-    progress++;
-  }
-  $("#realClickCount").text(clicks);
+    if (clicks >= 10 && progress == 0) {
+        unlockAutoClicker();
+        progress++;
+    }
+    if (autoClickers >= 30 && progress == 1) {
+        unlockMegaClicker();
+        progress++;
+    }
+    $("#realClickCount").text(clicks);
 }
 
 function updateMoney() {
-  $("#moneyCount").text(money);
-  if (money < autoClickerCost) {
-    $("#makeAutoClicker").prop('disabled', true);
-  } else {
-    $("#makeAutoClicker").prop('disabled', false);
-  }
-  if (money < megaClickerCost) {
-    $("#makeMegaClicker").prop('disabled', true);
-  } else {
-    $("#makeMegaClicker").prop('disabled', false);
-  }
+    $("#moneyCount").text(money);
+    if (money < autoClickerCost) {
+        $("#makeAutoClicker").prop('disabled', true);
+    } else {
+        $("#makeAutoClicker").prop('disabled', false);
+    }
+    if (money < megaClickerCost) {
+        $("#makeMegaClicker").prop('disabled', true);
+    } else {
+        $("#makeMegaClicker").prop('disabled', false);
+    }
 }
 
 function updateAutoClicker() {
-  $("#realAutoClickerCount").text(autoClickers);
+    if (autoClickers > 0) {
+        if(autoClickerInt != null) {
+            clearInterval(autoClickerInt);
+        }
+        autoClickerInt = setInterval(function () {
+            clicks++;
+            updateClicks();
+        }, 1000 / autoClickers);
+    }
+    $("#realAutoClickerCount").text(autoClickers);
 }
 
 function unlockAutoClicker() {
-  $("#main").append($("<fieldset id='autoClickers'><legend><b>Autoclickers: </b><span id='realAutoClickerCount'>0</span></legend><button id='makeAutoClicker'>+1 Autoclicker</button><p><b>Cost: </b><span id='autoClickerCost'>"+autoClickerCost+"</span></p></fieldset>"));
+    $("#main").append($("<fieldset id='autoClickers'><legend><b>Autoclickers: </b><span id='realAutoClickerCount'>0</span></legend><button id='makeAutoClicker'>+1 Autoclicker</button><p><b>Cost: </b><span id='autoClickerCost'>" + autoClickerCost + "</span></p></fieldset>"));
 }
 
 function updateAutoClickerCost() {
-  autoClickerCost = Math.round(autoClickerCost * autoClickerCostFact);
-  $("#autoClickerCost").text(autoClickerCost);
+    autoClickerCost = Math.round(autoClickerCost * autoClickerCostFact);
+    $("#autoClickerCost").text(autoClickerCost);
 }
 
 function updateMegaClicker() {
+    if (megaClickers > 0) {
+        if(megaClickerInt != null) {
+            clearInterval(megaClickerInt);
+        }
+        megaClickerInt = setInterval(function () {
+            clicks++;
+            updateClicks();
+        }, 1000 / (megaClickers*15));
+    }
     $("#realMegaClickerCount").text(megaClickers);
 }
 
 function unlockMegaClicker() {
-  $("#main").append($("<fieldset id='megaClickers'><legend><b>Megaclickers: </b><span id='realMegaClickerCount'>0</span></legend><button id='makeMegaClicker'>+1 Megaclicker</button><p><b>Cost: </b><span id='megaClickerCost'>"+megaClickerCost+"</span></p></fieldset>"));
+    $("#main").append($("<fieldset id='megaClickers'><legend><b>Megaclickers: </b><span id='realMegaClickerCount'>0</span></legend><button id='makeMegaClicker'>+1 Megaclicker</button><p><b>Cost: </b><span id='megaClickerCost'>" + megaClickerCost + "</span></p></fieldset>"));
 }
 
 function updateMegaClickerCost() {
-  megaClickerCost = Math.round(megaClickerCost * megaClickerCostFact);
-  $("#megaClickerCost").text(megaClickerCost);
+    megaClickerCost = Math.round(megaClickerCost * megaClickerCostFact);
+    $("#megaClickerCost").text(megaClickerCost);
 }
 
 function everySecondDo() {
-  if (autoClickers > 0) {
-    for (let i = 0; i < autoClickers; i++) {
-      clicks++;
-      updateClicks();
-    }
-  }
-  if(megaClickers > 0) {
-    for (let i = 0; i < megaClickers; i++) {
-      clicks+=15;
-      updateClicks();
-    }
-  }
+    console.log(clicks);
 }
 
 function updateAll() {
-  updateClicks();
-  updateMoney();
-  if (progress >= 1) {
-    unlockAutoClicker();
-  }
-  if(progress >= 2) {
-    unlockMegaClicker();
-  }
-  updateAutoClicker();
-  updateMegaClicker();
+    updateClicks();
+    updateMoney();
+    if (progress >= 1) {
+        unlockAutoClicker();
+    }
+    if (progress >= 2) {
+        unlockMegaClicker();
+    }
+    updateAutoClicker();
+    updateMegaClicker();
 }
 
 function saveGame() {
-  document.cookie = "clicks=" + clicks;
-  document.cookie = "money=" + money;
-  document.cookie = "autoClickers=" + autoClickers;
-  document.cookie = "autoClickerCost=" + autoClickerCost;
-  document.cookie = "megaClickers="+ megaClickers;
-  document.cookie = "megaClickerCost=" + megaClickerCost;
-  document.cookie = "progress=" + progress;
+    document.cookie = "clicks=" + clicks;
+    document.cookie = "money=" + money;
+    document.cookie = "autoClickers=" + autoClickers;
+    document.cookie = "autoClickerCost=" + autoClickerCost;
+    document.cookie = "megaClickers=" + megaClickers;
+    document.cookie = "megaClickerCost=" + megaClickerCost;
+    document.cookie = "progress=" + progress;
 }
 
 function loadGame() {
-  if (document.cookie != "") {
-    clicks = Number(getCookie("clicks"));
-    money = Number(getCookie("money"));
-    autoClickers = Number(getCookie("autoClickers"));
-    autoClickerCost = Number(getCookie("autoClickerCost"));
-    megaClickers = Number(getCookie("megaClickers"));
-    megaClickerCost = Number(getCookie("megaClickerCost"));
-    progress = Number(getCookie("progress"));
-    updateAll();
-  }
+    if (document.cookie != "") {
+        clicks = Number(getCookie("clicks"));
+        money = Number(getCookie("money"));
+        autoClickers = Number(getCookie("autoClickers"));
+        autoClickerCost = Number(getCookie("autoClickerCost"));
+        megaClickers = Number(getCookie("megaClickers"));
+        megaClickerCost = Number(getCookie("megaClickerCost"));
+        progress = Number(getCookie("progress"));
+        updateAll();
+    }
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
